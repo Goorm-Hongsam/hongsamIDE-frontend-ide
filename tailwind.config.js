14fd9db02d7aeafae1a17e178e59c58a106e826c
강경규 <qwzx16@naver.com>
2023-12-11 23:15:03 +0900

    CHORE: css파일 경로 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-color': '#04c92f',
+        'main-dark-color': '#16b438',
+        'ligth-gray': '#f3f3f3',
+        'dark-gray-color': '#b0b0b0',
+        'alert-color': '#ff6464',
+      },
+    },
   },
   plugins: [],
 };
