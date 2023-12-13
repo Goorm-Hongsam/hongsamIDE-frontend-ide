ef16128b868f4a030d5975c06f7b1726cd641ca0
강경규 <qwzx16@naver.com>
2023-12-13 18:33:45 +0900

    CHORE: msw setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/index.css';
+import { worker } from './utils/mooks/worker.ts';
+import { RecoilRoot } from 'recoil';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>,
+);
