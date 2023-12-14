af97a1a083df1ec2ca0998dbfb3a6154cc838924
강경규 <qwzx16@naver.com>
2023-12-14 13:13:21 +0900

    CHORE: context 테스트

---
+++ src/contexts/DarkModeContext.tsx
@@ -0,0 +1,22 @@
+import React, { createContext, useState, ReactNode } from 'react';
+
+interface DarkModeContextProps {
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+}
+
+const DarkModeContext = createContext<DarkModeContextProps | undefined>(undefined);
+
+export const DarkModeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
