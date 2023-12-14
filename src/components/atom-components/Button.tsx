1a8372fbc5385950d2a936c24f63165973a7a3c8
강경규 <qwzx16@naver.com>
2023-12-14 13:12:47 +0900

    FEAT: 버튼 생성

--- src/components/atom-components/Button.tsx
+++ src/components/atom-components/Button.tsx
@@ -1,36 +1,37 @@
-import React, { useEffect } from 'react';
-interface ButtonProps {
-  label: string;
-  tooltipElement?: JSX.Element;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+import { cva, VariantProps } from 'class-variance-authority';
+import { ButtonHTMLAttributes, FC } from 'react';
+import { cn } from '../../utils/cn';
+
+export const ButtonVariants = cva(
+  `flex items-center justify-center hover:text-main-color transition-all text-center relative`,
+  {
+    variants: {
+      variant: {
+        main: 'bg-main-color shadow-md',
+        white: 'bg-white',
+      },
+      size: {
+        rounded: 'w-7 h-7 rounded-full',
+      },
+    },
+  },
+);
+
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof ButtonVariants> {
+  label?: string;
+  children?: React.ReactElement;
+  additionalClass?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick, tooltipElement }) => {
-  const [isToolTip, setIsToolTip] = React.useState(false);
-  useEffect(() => {
-    if (isToolTip) {
-      if (isToolTip) {
-        setTimeout(() => {
-          setIsToolTip(false);
-        }, 2000);
-      }
-    }
-  }, [isToolTip]);
+const Button: FC<ButtonProps> = ({ variant, size, children, label, additionalClass, ...props }) => {
   return (
-    <div className='flex flex-col items-center'>
-      <button
-        onClick={() => {
-          if (onClick) {
-            onclick;
-          }
-          setIsToolTip(!isToolTip);
-        }}
-        className='hover:text-main-color'
-      >
-        {label}
-      </button>
-      {isToolTip && tooltipElement}
-    </div>
+    <button className={cn(ButtonVariants({ variant, size }), additionalClass)} {...props}>
+      {children && children}
+      {label && label}
+    </button>
   );
 };
+
 export default Button;
