646298437f4dd9b7cc97ab277de9fcfc769810fb
강경규 <qwzx16@naver.com>
2023-12-16 21:36:52 +0900

    FEAT: msx 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/index.css';
-import { worker } from './utils/mooks/worker.ts';
+
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start();
-}
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RecoilRoot>
     <BrowserRouter>
