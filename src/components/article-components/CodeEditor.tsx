1bad9038887164c7f9f6de317db71678217677b7
강경규 <qwzx16@naver.com>
2024-02-17 03:04:57 +0900

    TEST: 언어에 대한 설정을 자바로 임시로 고정해두었습니다.

--- src/components/article-components/CodeEditor.tsx
+++ src/components/article-components/CodeEditor.tsx
@@ -5,23 +5,37 @@ import { EditorView } from '@uiw/react-codemirror';
 import { githubDark, githubLight } from '@uiw/codemirror-theme-github';
 import { java } from '@codemirror/lang-java';
 
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { codeState, isDarkModeState } from '../../atoms/recoliAtoms';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { codeState, isDarkModeState, languageState } from '../../atoms/recoliAtoms';
 
 export interface ReactCodeMirrorRef {
   editor?: HTMLDivElement | null;
   state?: EditorState;
   view?: EditorView;
 }
+// const langExtens = () => {
+//   const javaLang = [java()];
+//   const langMap = new Map();
+//   langMap.set('java', javaLang);
+//   return langMap;
+// };
 
 const CodeEditor: React.FC = () => {
-  // const pyLang = [python()];
-  // const jsLang = [javascript()];
   const javaLang = [java()];
+  // const [lang, setLang] = React.useState('');
+  // React.useEffect(() => {
+  //   const lang = langExtens();
+
+  //   console.log(lang.get('java'));
+  // }, []);
+
+  const setLanguage = useSetRecoilState(languageState);
 
   const [code, setCode] = useRecoilState(codeState);
   const isDarkMode = useRecoilValue(isDarkModeState);
-
+  React.useEffect(() => {
+    setLanguage('java');
+  }, []);
   const onChange = React.useCallback((val: string) => {
     setCode(val);
   }, []);
