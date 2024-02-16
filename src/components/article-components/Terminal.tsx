c4c178d71823aec9382915fba5f32773a65d324a
강경규 <qwzx16@naver.com>
2024-02-17 03:07:09 +0900

    FEAT: 터미널 전반적인 로직와 API 엔드포인트를 수정했습니다.
    - 이제 result와 resultObj는 별도의 값을 갖습니다.
    - 서버에서 코드에 대한 단순 텍스트는 result에서 처리해서 보여줍니다.
    - 에러 및 코드 실행과 제출에 대한 결과에 대한 것은 resultObj에서 처리하여 보여줍니다.
    - ngrok 테스트 결과 정상적으로 작동합니다.
    - ** 개선할 점은 시간 정보를 이용하여 점진적으로 결과를 보여주는 것
    - ** 제출 시에는 모달을 띄워줄 것

--- src/components/article-components/Terminal.tsx
+++ src/components/article-components/Terminal.tsx
@@ -1,28 +1,103 @@
 import { cn } from '../../utils/cn';
 
-import LodadingIcon from '../atom-components/icon/LoadingIcon';
+import LoadingIcon from '../atom-components/icon/LoadingIcon';
 import useCodeSubmit from '../../hooks/useCodeSubmit';
 import useDarkMode from '../../hooks/useDarkMode';
-import { useEffect, useState } from 'react';
+
+import { useRecoilValue } from 'recoil';
+import { isResultLoadingState } from '../../atoms/recoliAtoms';
 
 const Terminal = () => {
-  const { result } = useCodeSubmit();
-  const [isLoading, setIsLoading] = useState(false);
+  const { result, resultObj } = useCodeSubmit();
   const { darkModeClasses } = useDarkMode();
-  useEffect(() => {
-    if (result === '') {
-      setIsLoading(true);
-    } else {
-      setIsLoading(false);
-    }
-  }, [result]);
-  return (
-    <div
-      style={{ height: '203px' }}
-      className={cn('bottom-0 right-0 mb-12 bg-white p-5', darkModeClasses)}
-    >
-      {isLoading ? <LodadingIcon /> : <p>{result}</p>}
-    </div>
-  );
+  const isResultLoading = useRecoilValue(isResultLoadingState);
+
+  if (isResultLoading) {
+    return (
+      <div
+        style={{ height: '203px' }}
+        className={cn(
+          'bottom-0 right-0 mb-12 bg-white p-5 overflow-y-auto flex flex-col gap-5',
+          darkModeClasses,
+        )}
+      >
+        <LoadingIcon />
+      </div>
+    );
+  }
+
+  if (result !== '') {
+    return (
+      <div
+        style={{ height: '203px' }}
+        className={cn(
+          'bottom-0 right-0 mb-12 bg-white p-5 overflow-y-auto flex flex-col gap-5',
+          darkModeClasses,
+        )}
+      >
+        {result}
+      </div>
+    );
+  }
+  if (resultObj.errorMessage) {
+    return (
+      <div
+        style={{ height: '203px' }}
+        className={cn(
+          'bottom-0 right-0 mb-12 bg-white p-5 overflow-y-auto flex flex-col gap-5',
+          darkModeClasses,
+        )}
+      >
+        {resultObj.errorMessage}
+      </div>
+    );
+  }
+
+  if (resultObj) {
+    return (
+      <div
+        style={{ height: '203px' }}
+        className={cn(
+          'bottom-0 right-0 mb-12 bg-white p-5 overflow-y-auto flex flex-col gap-5',
+          darkModeClasses,
+        )}
+      >
+        {resultObj.printResult.map((_, i) => {
+          return (
+            <div key={i} className='flex flex-col'>
+              <p className='text-lg'>테스트 {i + 1}</p>
+              <div className='flex gap-5'>
+                <p className='text-slate-500'>입력 값</p>
+                <p></p>
+              </div>
+              <div className='flex gap-5'>
+                <p className='text-slate-500'>기댓 값</p>
+                <p>{resultObj.originAnswer[i]}</p>
+              </div>
+              <div className='flex gap-5'>
+                <p className='text-slate-500'>실행 결과</p>
+                {resultObj.correctResult[i] ? (
+                  <p className='text-blue-400'>테스트케이스 정답입니다.</p>
+                ) : (
+                  <p className='text-red-400'>
+                    실행한 출력 값 "{resultObj.printResult[i]}"와 기댓 값 "
+                    {resultObj.originAnswer[i]}" 가 다릅니다
+                  </p>
+                )}
+              </div>
+              <div className='flex gap-5'>
+                <p className='text-slate-500'>출력</p>
+                <p>{resultObj.printResult[i]}</p>
+              </div>
+              <div className='flex gap-5'>
+                <p className='text-slate-500'>소요 시간</p>
+                <p>{resultObj.timeResult[i]}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 };
 export default Terminal;
--- src/hooks/useCodeSubmit.ts
+++ src/hooks/useCodeSubmit.ts
@@ -1,8 +1,10 @@
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import {
   codeState,
+  isResultLoadingState,
   languageState,
   questionIdState,
+  resultObjState,
   resultState,
   uuidState,
 } from '../atoms/recoliAtoms';
@@ -15,31 +17,66 @@ const useCodeSubmit = () => {
   const questionId = useRecoilValue(questionIdState);
   const language = useRecoilValue(languageState);
   const [result, setResult] = useRecoilState(resultState);
+  const [resultObj, setResultObj] = useRecoilState(resultObjState);
+  const setIsResultLoading = useSetRecoilState(isResultLoadingState);
 
   const submitCode = async () => {
     setResult('');
+    setIsResultLoading(true);
     try {
-      const result = await defaultAxios.post('ide/run', {
+      const result = await defaultAxios.post('grader/run/lee', {
         uuid: uuid,
         questionId: questionId,
         requestCode: code,
         language: language,
+        gradeType: 'all',
       });
-      setResult(result.data);
+      console.log(result);
+      setResult('');
+      setIsResultLoading(false);
+      setResultObj(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const testCode = async () => {
+    setResult('');
+    setIsResultLoading(true);
+    try {
+      const result = await defaultAxios.post('grader/run/lee', {
+        questionId: questionId,
+        uuid: uuid,
+        requestCode: code,
+        language: language,
+        gradeType: 'example',
+      });
+      setResult('');
+      setIsResultLoading(false);
+      setResultObj(result.data);
     } catch (error) {
       console.log(error);
     }
   };
   const saveCode = async () => {
     setResult('');
+    setIsResultLoading(true);
+    setResultObj({
+      compileError: false,
+      correctResult: [],
+      errorMessage: null,
+      originAnswer: [],
+      printResult: [],
+      timeResult: [],
+    });
     try {
-      const result = await defaultAxios.post('ide/save', {
+      const result = await defaultAxios.post('grader/save/lee', {
         uuid: uuid,
         questionId: questionId,
         requestCode: code,
         language: language,
       });
-      setResult(result.data);
+      setIsResultLoading(false);
+      setResult(result.data.message);
     } catch (error) {
       console.log(error);
     }
@@ -47,23 +84,29 @@ const useCodeSubmit = () => {
   const fetchCode = async (uuidParam: string, questionIdParam: string) => {
     setResult(`주석을 보고 코드 작성 방법을 이해한 후에 문제를 풀어보세요 !`);
     try {
-      const result = await defaultAxios.post('ide/latest', {
+      const result = await defaultAxios.post('grader/get/lee', {
         uuid: uuidParam,
         questionId: questionIdParam,
+        language: language,
       });
+
       if (result.status === 200) {
-        const requestCode = result.data;
+        const requestCode = result.data.requestCode;
         setCode(requestCode);
+        setIsResultLoading(false);
       }
       if (result.status === 500) {
         setCode(javaDefaultValue(questionId));
+        setIsResultLoading(false);
       }
     } catch (error) {
       console.log(error);
     }
   };
 
-  return { submitCode, saveCode, fetchCode, result };
+  // React.useEffect(()=>{},[result, resultObj])
+
+  return { submitCode, saveCode, fetchCode, result, testCode, resultObj };
 };
 
 export default useCodeSubmit;
