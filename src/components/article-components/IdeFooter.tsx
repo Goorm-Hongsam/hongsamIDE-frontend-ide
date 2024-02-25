144fca9c3c8bca48b1708534791849cff56810f1
강경규 <qwzx16@naver.com>
2024-02-25 20:38:34 +0900

    FIX: chat 기능 임시로 삭제

--- src/components/article-components/IdeFooter.tsx
+++ src/components/article-components/IdeFooter.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import Button from '../atom-components/Button';
-import Chat from './Chat/Chat';
+
 import { isDarkModeState } from '../../atoms/recoliAtoms';
 import { useRecoilValue } from 'recoil';
 import { cn } from '../../utils/cn';
@@ -9,12 +9,12 @@ import { copyUrlToClipboard } from '../../utils/copyUrl';
 
 const IdeFooter: FC = () => {
   const [urlCopideView, setUrlCopideView] = React.useState<boolean>(false);
-  const [isChatView, setIsChatView] = React.useState(false);
+  // const [isChatView, setIsChatView] = React.useState(false);
 
   const isDarkMode = useRecoilValue(isDarkModeState);
-  const openChating = () => {
-    setIsChatView(!isChatView);
-  };
+  // const openChating = () => {
+  //   setIsChatView(!isChatView);
+  // };
 
   React.useEffect(() => {
     if (urlCopideView) {
@@ -26,18 +26,18 @@ const IdeFooter: FC = () => {
   return (
     <div
       className={cn(
-        'flex h-12 gap-5 fixed bottom-0 z-50 w-screen pr-5 border-t border-main-color bg-white',
+        'flex h-12 gap-5 fixed bottom-0 z-20 w-screen pr-5 border-t border-main-color bg-white',
         isDarkMode ? 'bg-black text-white' : 'bg-white',
       )}
     >
       <div className='grow relative' />
-      {isChatView && <Chat />}
+      {/* {isChatView && <Chat />} */}
       <div className='h-full flex items-center justify-center'>
         {urlCopideView && <ToolTip label='복사 완료' position={'top'} />}
 
         <Button label='공유' onClick={() => copyUrlToClipboard(setUrlCopideView)} />
       </div>
-      <Button label='채팅' onClick={openChating} />
+      {/* <Button label='채팅' onClick={openChating} /> */}
     </div>
   );
 };
