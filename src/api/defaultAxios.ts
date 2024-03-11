7f8f81751fad93f1f1420218c6e0818bc5d22842
강경규 <qwzx16@naver.com>
2024-03-11 17:58:49 +0900

    CHORE: 최종 작업 끝

--- src/api/defaultAxios.ts
+++ src/api/defaultAxios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const defaultAxios = axios.create({
-  baseURL: 'https://gateway.hong-sam.online/',
+  baseURL: 'https://gateway.hong-sam.online',
   headers: {
     Authorization: 'Bearer your-token',
   },
