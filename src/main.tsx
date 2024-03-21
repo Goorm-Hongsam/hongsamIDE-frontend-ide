495c29e18e278cdb8a7db0518c6319f534ee33e5
강경규 <qwzx16@naver.com>
2024-03-22 01:35:56 +0900

    CHORE: cookie provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,17 @@ import './styles/index.css';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { CookiesProvider } from 'react-cookie';
 const queryClinent = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClinent}>
     <RecoilRoot>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <CookiesProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CookiesProvider>
     </RecoilRoot>
   </QueryClientProvider>,
 );
