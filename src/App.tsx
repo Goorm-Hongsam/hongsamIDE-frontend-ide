192c2bc0cbf6e328dce3e853ef36a1d9ea83ba95
강경규 <qwzx16@naver.com>
2024-03-22 01:36:13 +0900

    FEAT: get cookie 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import './styles/App.css';
 import Main from './pages/Main';
 import { Route, Routes } from 'react-router-dom';
 import Guest from './pages/Guest';
-import React from 'react';
 import { useCookies } from 'react-cookie';
+import { getCookie } from './utils/cookie';
 
 function App() {
-  const [cookies] = useCookies(['Authorization']);
-
+  const [cookies] = useCookies();
+  console.log(cookies);
+  console.log(getCookie('Authorization'));
   React.useEffect(() => {
     const token = cookies.Authorization;
     console.log(token);
@@ -15,7 +17,7 @@ function App() {
       // 토큰이 존재하면 로컬 스토리지에 저장
       localStorage.setItem('token', token);
     }
-  }, [cookies.Authorization]);
+  }, []);
 
   return (
     <React.Fragment>
