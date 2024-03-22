5a75d59f25b65b71e4f9d6f954d4f6b459b29593
강경규 <qwzx16@naver.com>
2024-03-22 15:30:57 +0900

    FEAT: axios 헤더에 로컬스토리지 값 담는 로직 생성

--- src/api/defaultAxios.ts
+++ src/api/defaultAxios.ts
@@ -2,10 +2,20 @@ import axios from 'axios';
 
 const defaultAxios = axios.create({
   baseURL: 'https://gateway.hong-sam.online',
-  headers: {
-    Authorization: 'Bearer your-token',
-  },
   withCredentials: true,
 });
+
+defaultAxios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('Authorization');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default defaultAxios;
