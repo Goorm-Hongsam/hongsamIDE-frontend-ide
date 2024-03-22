9c2b2abe061012cc964b83cfa232f2ff60fa09af
강경규 <qwzx16@naver.com>
2024-03-22 16:38:27 +0900

    FEAT: 최초 진입 토큰 설정

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,15 @@ import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { CookiesProvider } from 'react-cookie';
+import { getCookie } from './utils/cookie.ts';
 const queryClinent = new QueryClient();
+
+const token2 = getCookie('Authorization');
+console.log('main.tsx token2 : ', token2);
+if (token2) {
+  localStorage.setItem('Authorization', token2);
+}
+console.log('main.tsx Authorization LocalStorage : ', localStorage.getItem('Authorization'));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClinent}>
