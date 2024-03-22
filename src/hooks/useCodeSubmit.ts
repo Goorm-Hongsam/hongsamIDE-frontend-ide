983f8bbec4ebd0d28f377a9c4cc4f86fecae98f8
강경규 <qwzx16@naver.com>
2024-03-22 20:37:07 +0900

    FEAT: 예외사항 코드 임시 작성

--- src/hooks/useCodeSubmit.ts
+++ src/hooks/useCodeSubmit.ts
@@ -89,22 +89,37 @@ const useCodeSubmit = () => {
         questionId: questionIdParam,
         language: language,
       });
-
       if (result.status === 200) {
         const requestCode = result.data.requestCode;
         setCode(requestCode);
         setIsResultLoading(false);
       }
-      if (result.status === 500) {
+      //최초 문제 진입
+      if (result.status === 502) {
         setCode(javaDefaultValue(questionId));
+        setResult(`주석을 보고 코드 작성 방법을 이해한 후에 문제를 풀어보세요 !`);
         setIsResultLoading(false);
+      }
+      /**
+       * 토큰 헤더에 포함 안되었거나, uuid 비교했을 때 실패(다른 사람의 문제풀이 페이지에 접근한 경우) : 403
+       * 토큰 유효성 검증 실패 : 401
+       */
+      if (result.status === 403) {
+        setIsResultLoading(false);
+        setResult('본인의 문제에만 접근할 수 있습니다.');
+      }
+      if (result.status === 401) {
+        setIsResultLoading(false);
+        setResult(
+          '죄송합니다. 현재 세션이 만료되었거나 인증에 실패했습니다. 다시 로그인하고 시도해주세요.',
+        );
       }
     } catch (error) {
       console.log(error);
+      setIsResultLoading(false);
+      setResult('예상치 못한 문제가 발생했습니다.');
     }
   };
-
-  // React.useEffect(()=>{},[result, resultObj])
 
   return { submitCode, saveCode, fetchCode, result, testCode, resultObj };
 };
